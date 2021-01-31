331d90796b62025a18e8b8a4bc4118936ba7bd40
Tiffany Zhu <zhz90@pitt.edu>
2021-01-31 17:01:53 -0500

    Add files via upload

---
+++ Menu Manager Application/Dessert.java
@@ -0,0 +1,16 @@
+package edu.pitt.is17.zhz90.menumanager;
+
+/**
+ * Class Dessert
+ * @author Zhehan Zhu
+ * Created: 10/13/2016
+ */
+
+public class Dessert extends MenuItem {
+	
+	//Constructor 
+	public Dessert(String name, String desc, int cal, double price){
+		super(name, desc, cal, price);
+	}
+
+}
